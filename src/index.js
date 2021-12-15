90d879eea5ea4376fcd308d9be0b5e4f0e935f36
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-15 10:03:30 -0500

    added the plan to index.js, renamed the function

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ async function fetchGiffy() {
   const queryData = await response.json();
   image.src = queryData.data.images.original.url;
 }
+
 const fetchGiffyBtn = document.querySelector('.fetch-giffy');
 fetchGiffyBtn.addEventListener('click', () => {
   fetchGiffy();
   //    later we can add a parameter to fetchGiffy() to display for example,
   //    a clear weather giff, for now it will display something related to sample
+});
+async function fetchWeather() {
+  const weatherParagraph = document.querySelector('.weather');
+  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Miami&APPID=ea13d76af44f95e6e901a4a69585bb22', { mode: 'cors' });
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+const fetchWeatherBtn = document.querySelector('.fetch-weather');
+fetchWeatherBtn.addEventListener('click', () => {
+  fetchWeather();
 });
