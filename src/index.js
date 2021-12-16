b03efa0c9612d88cf00a662aa10f0730fac58661
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-16 11:14:52 -0500

    added a swtich statement that checks what option the dropdown has selected, and then sends the corresponding fetch request

--- src/index.js
+++ src/index.js
@@ -13,14 +13,29 @@ fetchGiffyBtn.addEventListener('click', () => {
   //    later we can add a parameter to fetchGiffy() to display for example,
   //    a clear weather giff, for now it will display something related to sample
 });
-async function fetchWeather() {
+async function fetchWeather(fetchRequest) {
   const weatherParagraph = document.querySelector('.weather');
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Miami&APPID=ea13d76af44f95e6e901a4a69585bb22', { mode: 'cors' });
+  const response = await fetch(fetchRequest, { mode: 'cors' });
   const weatherData = await response.json();
   console.log(weatherData);
 }
 const fetchWeatherBtn = document.querySelector('.fetch-weather');
 fetchWeatherBtn.addEventListener('click', () => {
-  fetchWeather();
+  const dropdownBtn = document.querySelector('#How-would-you-like-to-find-the-weather-dropDownButton');
+  switch (dropdownBtn.textContent) {
+    case 'city name':
+      // eslint-disable-next-line no-case-declarations
+      const cityName = document.querySelector('.city-name');
+      fetchWeather(`http://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&APPID=ea13d76af44f95e6e901a4a69585bb22`);
+      break;
+    case 'zip code and country':
+      break;
+    case 'longitude and latitude':
+      fetchWeather('');
+      break;
+    default:
+      fetchWeather('http://api.openweathermap.org/data/2.5/weather?q=Miami&APPID=ea13d76af44f95e6e901a4a69585bb22');
+      break;
+  }
 });
 createForm();
