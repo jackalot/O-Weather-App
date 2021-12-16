590e04b6e0819a18969dd93a67442402040f11d6
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-16 08:08:24 -0500

    a label will now be created with createForm

--- src/createForm.js
+++ src/createForm.js
@@ -3,5 +3,13 @@ import createDropdown from '@jackalot/createdropdown.js';
 function createForm() {
   const body = document.querySelector('body');
   createDropdown(body, 'How would you like to find the weather', ['city name', 'zip code and country', 'longitude and latitude']);
+  function createNewLabel(labelText) {
+    const x = document.createElement('LABLEL');
+    const y = document.createTextNode(labelText);
+    x.setAttribute('for', labelText);
+    x.append(y);
+    body.appendChild(x);
+  }
+  createNewLabel('this is a test');
 }
 export default createForm;
