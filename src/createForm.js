22029855cd1f7a8fd180ffb0088f803d8aca35ef
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-16 09:50:50 -0500

    added the ability to remove fields using promises, but not labels

--- src/createForm.js
+++ src/createForm.js
@@ -15,19 +15,30 @@ function createForm() {
     const joined = split.join('-');
     const field = document.createElement('input');
     field.type = fieldType;
-    field.classList.add(joined, 'Field-entry');
+    field.classList.add(joined, 'Field-Entry');
     field.setAttribute('name', fieldName);
     body.appendChild(field);
+  }
+  function removeFields(exception1, exception2) {
+    const allFields = document.querySelectorAll('.Field-Entry');
+    allFields.forEach((element) => {
+      if (element.textContent !== exception1 || element.textContent !== exception2) {
+        const label = document.querySelector(element.name);
+        element.parentNode.removeChild(element);
+      }
+    });
   }
   const dropdownOptions = document.querySelectorAll('.dropDown-Options');
   dropdownOptions.forEach((element) => {
     element.addEventListener('click', () => {
       console.log('click');
       if (element.textContent === 'zip code and country') {
-        createNewLabel('zip code');
-        createNewField('zip code', 'Number');
-        createNewLabel('country');
-        createNewField('country', 'Text');
+        removeFields('zip code', 'country').then(() => {
+          createNewLabel('zip code');
+          createNewField('zip code', 'Number');
+          createNewLabel('country');
+          createNewField('country', 'Text');
+        });
       } else if (element.textContent === 'longitude and latitude') {
         createNewLabel('longitude');
         createNewField('longitude', 'Number');
